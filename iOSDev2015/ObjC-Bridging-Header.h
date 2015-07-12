5a801263ace4960fab9784be8e0c03f3c633e561
Adam Van Prooyen <adam.vanpr@gmail.com>
2015-07-11 17:27:12 -0700

    Added Bridge Header, initial FactualAPI support, and revamped notification creation flow.

---
+++ iOSDev2015/ObjC-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  ObjC-Bridging-Header.h
+//  iOSDev2015
+//
+//  Created by Adam Van Prooyen on 7/11/15.
+//  Copyright (c) 2015 Daniel Richards. All rights reserved.
+//
+
+#import <FactualSDK/FactualAPI.h>
--- iOSDev2015/ViewController.swift
+++ iOSDev2015/ViewController.swift
@@ -38,6 +38,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     override func viewDidAppear(animated: Bool) {
         TRUE
+        
+        FactualRowFilter.fieldName("adfa", equalTo: "adfdas")
     }
 }
 
